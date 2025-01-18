697435ef56db94b5782fe8bc4960b40809675e4e
MuradZakaria <murad.zakaria88@gmail.com>
2025-01-18 13:56:25 +0200

    some init msg

---
+++ lib/features/dashboard/views/dashboard_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub_dashboard/features/dashboard/views/dashboard_view_body.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+static const String routeName = 'dashboard';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: DashboardViewBody(),
+    );
+  }
+}
